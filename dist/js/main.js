3a176952a8fbf8d58bc7daf8e3cb1b5203cdd3e9
hasegawa2073 <hasetatsu2001@icloud.com>
2022-09-04 20:15:14 +0900

    add: 握っているリストがドラッグしたマウスに沿って動くところまで

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,13 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', function () {
     const todoLists = document.querySelectorAll('.todo__li');
-    const gripDelay = 300;
+    const gripDelay = 200;
     let startTimeMousedown;
     let endTimeMousedown;
+    let startMouseX;
+    let startMouseY;
+    let currentMouseX;
+    let currentMouseY;
     const defaultStyle = (target) => {
         target.classList.remove('grip-start');
         target.classList.remove('grip');
@@ -11,7 +15,10 @@ document.addEventListener('DOMContentLoaded', function () {
     todoLists.forEach((list) => {
         list.addEventListener('mousedown', function (e) {
             startTimeMousedown = e.timeStamp;
+            startMouseX = e.clientX;
+            startMouseY = e.clientY;
             const target = e.currentTarget;
+            // マウス押下時にgrip-startクラスを付与してマウス押下のリアクションを返す
             target.classList.add('grip-start');
             const removeGripStart = new Promise((resolve, reject) => {
                 setTimeout(() => {
@@ -24,14 +31,26 @@ document.addEventListener('DOMContentLoaded', function () {
                 target.classList.add('grip');
             });
         });
+        list.addEventListener('mousemove', function (e) {
+            currentMouseX = e.clientX;
+            currentMouseY = e.clientY;
+            const diffMouseX = currentMouseX - startMouseX;
+            const diffMouseY = currentMouseY - startMouseY;
+            const target = e.currentTarget;
+            // リストを掴んだ状態mousemoveしてるとき
+            if (target.classList.contains('grip')) {
+                target.style.transform = `translateY(${diffMouseY}px)`;
+            }
+        });
         list.addEventListener('mouseup', function (e) {
             endTimeMousedown = e.timeStamp;
             let timeMousedown = endTimeMousedown - startTimeMousedown;
             let timeDiffDelay = gripDelay - timeMousedown;
             const target = e.currentTarget;
             defaultStyle(target);
+            target.style.transform = '';
             // リストが浮き上がるアニメーションが始まる前にマウスの押下をやめたとき
-            if (timeMousedown <= gripDelay) {
+            if (timeMousedown < gripDelay) {
                 setTimeout(() => {
                     defaultStyle(target);
                 }, timeDiffDelay + 10);
@@ -40,6 +59,7 @@ document.addEventListener('DOMContentLoaded', function () {
         list.addEventListener('mouseleave', function (e) {
             const target = e.currentTarget;
             defaultStyle(target);
+            target.style.transform = '';
         });
     });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
   const todoLists = document.querySelectorAll('.todo__li');
-  const gripDelay: number = 300;
+  const gripDelay: number = 200;
   let startTimeMousedown: number;
   let endTimeMousedown: number;
+  let startMouseX: number;
+  let startMouseY: number;
+  let currentMouseX: number;
+  let currentMouseY: number;
   const defaultStyle = (target: Element) => {
     target.classList.remove('grip-start');
     target.classList.remove('grip');
   };
   todoLists.forEach((list) => {
-    list.addEventListener('mousedown', function (e) {
+    list.addEventListener('mousedown', function (e: any) {
       startTimeMousedown = e.timeStamp;
+      startMouseX = e.clientX;
+      startMouseY = e.clientY;
       const target = e.currentTarget as Element;
+      // マウス押下時にgrip-startクラスを付与してマウス押下のリアクションを返す
       target.classList.add('grip-start');
       const removeGripStart = new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -23,22 +30,35 @@ document.addEventListener('DOMContentLoaded', function () {
         target.classList.add('grip');
       });
     });
+    list.addEventListener('mousemove', function (e: any) {
+      currentMouseX = e.clientX;
+      currentMouseY = e.clientY;
+      const diffMouseX = currentMouseX - startMouseX;
+      const diffMouseY = currentMouseY - startMouseY;
+      const target = e.currentTarget as HTMLElement;
+      // リストを掴んだ状態mousemoveしてるとき
+      if (target.classList.contains('grip')) {
+        target.style.transform = `translateY(${diffMouseY}px)`;
+      }
+    });
     list.addEventListener('mouseup', function (e) {
       endTimeMousedown = e.timeStamp;
       let timeMousedown: number = endTimeMousedown - startTimeMousedown;
       let timeDiffDelay = gripDelay - timeMousedown;
-      const target = e.currentTarget as Element;
+      const target = e.currentTarget as HTMLElement;
       defaultStyle(target);
+      target.style.transform = '';
       // リストが浮き上がるアニメーションが始まる前にマウスの押下をやめたとき
-      if (timeMousedown <= gripDelay) {
+      if (timeMousedown < gripDelay) {
         setTimeout(() => {
           defaultStyle(target);
         }, timeDiffDelay + 10);
       }
     });
     list.addEventListener('mouseleave', function (e) {
-      const target = e.currentTarget as Element;
+      const target = e.currentTarget as HTMLElement;
       defaultStyle(target);
+      target.style.transform = '';
     });
   });
 });
